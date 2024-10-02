250dec2bd78b8bda8d5ff2acf7f8ba1253afb3da
RyanWoodcockNorris <rdnorris04@gmail.com>
2024-10-02 12:03:58 +0100

    Update leetcode.cpp
    Solution 2980; Check if Bitwise OR Has Trailing Zeros

--- leetcode.cpp
+++ leetcode.cpp
@@ -695,4 +695,19 @@ public:
         return result;  
     }
 };
+    // Solution 2980; Check if Bitwise OR Has Trailing Zeros
+    class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                int result = nums[i] | nums[j];
+                if(result%2==0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
 }
